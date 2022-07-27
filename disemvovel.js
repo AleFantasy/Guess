71a0cdc2a65fe0f69d0105dff90637c11e4bea71
AleFantasy <lazarev.nazaR@gmail.com>
2022-07-27 22:27:46 +0300

    codewars

---
+++ disemvovel.js
@@ -0,0 +1,7 @@
+// function that takes a string and return a new string with all vowels removed.
+
+function disemvowel(str) {
+ 
+    str=str.replace(/[aieouAIEOU]/g,'');
+      return str;
+        }
